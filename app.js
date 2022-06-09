334d68434cf58e90f965774a101c2b66d67a7998
HAMIDREZA HASHEMI <HH7192@gmail.com>
2022-06-09 11:07:57 +0430

    to do list.

---
+++ app.js
@@ -0,0 +1,95 @@
+// Grabbing elements from DOM
+const list = document.querySelector("#todo-list");
+const userInput = document.querySelector("#todo-item");
+const addButton = document.querySelector("#add-button");
+const blankError = document.querySelector("#error");
+
+// Test Data
+const todolist = [
+  {
+    text: "wash the car",
+    isDone: false,
+  },
+  {
+    text: "walk the dog",
+    isDone: true,
+  },
+];
+
+// function: updateUI()
+function updateUI() {
+  // create temp var (garbage collector)
+  let result = "";
+
+  //   make modification to result based on the array
+  todolist.forEach((item, i) => {
+    // console.log("****************");
+    // console.log(item);
+    result += `
+    <tr>
+        <th class='${item.isDone === true ? "done" : ""}'>${item.text}</th>
+        <th>${item.isDone === true ? "Done" : "Pending"} </th>
+        <th>
+            <button class="btn btn-outline-success btn-sm" onclick=taskDone(${i})>
+                <i class="fa fa-check" aria-hidden="true"></i>
+            </button>
+        </th>
+        <th>
+            <button class="btn btn-outline-danger btn-sm" onclick=deleteTask(${i})>X</button>
+        </th>
+    </tr>
+    `;
+  });
+
+  //   clear input
+  userInput.value = "";
+
+  //   show the result on the DOM
+  list.innerHTML = result;
+}
+
+updateUI();
+
+// function: addToDo() : checks input value & add it to array
+function addToDo(e) {
+  e.preventDefault();
+  let userInputVal = userInput.value;
+
+  //   input validation
+  if (!userInputVal)
+    return (
+      (blankError.innerHTML = "Please Enter Your To Do Task!"),
+      setTimeout(() => blankError.remove(), 3000)
+    );
+
+  // put user input value in new object
+  const task = {
+    text: userInputVal,
+    isDone: false,
+  };
+
+  //   append object to array
+  todolist.push(task);
+
+  updateUI();
+}
+
+// function: taskDone()
+function taskDone(i) {
+  todolist[i].isDone = !todolist[i].isDone;
+  updateUI();
+}
+
+// function: deleteTask()
+function deleteTask(i) {
+  todolist.splice(i, 1);
+  updateUI();
+}
+
+// button event
+addButton.onclick = addToDo;
+
+// add on enter key event
+userInput.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") addToDo;
+});
